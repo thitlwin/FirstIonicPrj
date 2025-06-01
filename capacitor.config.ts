a8e97d86c33a771533105128fbc5c2ffaf03d60a
thitlwin <saithitlwin@gmail.com>
2025-06-01 13:04:16 +0700

    add liveupdate

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId:  'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '042a1261',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
